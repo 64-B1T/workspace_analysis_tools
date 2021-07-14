e62dabfc9d42c991fe4da24ce02ff5a2b3a773fe
li026833 <liam.chapin@maxar.com>
2021-07-14 08:41:14 -0600

    Update to workspace_command_line to address comments

--- workspace_command_line.py
+++ workspace_command_line.py
@@ -15,13 +15,15 @@ from workspace_helper_functions import load_point_cloud_from_file, sort_cloud
 import importlib
 import pickle
 
-#The ALPHA_VALUE is the alpha parameter  which determines which points are included or
+#The ALPHA_VALUE is the alpha parameter which determines which points are included or
 # excluded to create an alpha shape. This is technically an optional parameter,
 # however when the alpha shape optimizes this itself, for large clouds of points
 # it can take many hours to reach a solution. 1.2 is a convenient constant tested on a variety
 # of robot sizes.
 ALPHA_VALUE = 1.2
 
+#The TRANSPARENCY_CONSTANT determines the degree of transparency at which 3d objects are shown
+# when plotted within matplotlib.
 TRANSPARENCY_CONSTANT = .5
 
 
